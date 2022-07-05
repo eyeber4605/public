82c90e211465dc394a110a62d2dc8e4b93d0354e
eyeber4605 <55225055+eyeber4605@users.noreply.github.com>
2022-07-05 15:05:20 +0900

    Update click.function.js

--- assets/js/click.function.js
+++ assets/js/click.function.js
@@ -1,13 +1,14 @@
 $(function(){
-
-    var agent = navigator.userAgent.toLowerCase();
-    // 크롬 브라우저 확인
-    if( agent.indexOf("chrome") == -1) {
-        alert('Chrome 브라우저가 아닐경우 정상 작동 하지않을 수 있습니다. ');
-    }
+    // load main_init()
+    init.main_init();
 
     $("#startBtn").click(function () {
         trialBoot();
     });
+
+    $("#shareUrlCopy").click(function(){
+        init.shareUrlCopy();
+    });
+
     
 })
